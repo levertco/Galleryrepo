ec0d6881a52746d4fbfd9755c08e91519fbdb1bc
Levert <levertco@gmail.com>
2019-09-11 10:20:35 +0300

    deployment to heroku

--- pixels/settings.py
+++ pixels/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'photos',
-    "bootstrap4",
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
